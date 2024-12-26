cdbf4be9088f6d2e4e9f91954695e84bb3cd9555
Daniel Gorbachev <dg186533@gmail.com>
2024-12-26 18:59:05 -0500

    started working on heap datastructures

---
+++ Data Structures/Heaps/heaps.js
@@ -0,0 +1,34 @@
+class MaxBinaryHeap{
+    constructor(){
+        this.values = [41,39,33,18,27,12]
+    }
+
+    insert(val){
+        this.values.push(val)
+
+        function bubbleup(){
+            let idx = this.values.length - 1
+            const element = this.value[this.values.length - 1]
+            while(true){
+                let parentIdx = Math.floor((idx - 1)/2)
+                let parent = this.values[parentIdx]
+                if(element > parent){
+                    [this.values[parentIdx], this.values[idx]] = [this.values[idx], this.values[parentIdx]]
+                    idx = parentIdx
+                }
+                else{
+                    break
+                }
+            }
+        }
+
+        bubbleup()
+    }
+}
+
+let heap = new MaxBinaryHeap()
+
+heap.insert(55)
+
+console.log(heap)
+
