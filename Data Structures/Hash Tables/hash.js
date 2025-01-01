57cb1eeb3463d95f19a8e9c66662939510ec33e0
Daniel Gorbachev <dg186533@gmail.com>
2025-01-01 17:01:30 -0500

    added set and get methods to the hash function

--- Data Structures/Hash Tables/hash.js
+++ Data Structures/Hash Tables/hash.js
@@ -10,3 +10,43 @@ function hash(key, arrayLen){
 }
 
 console.log(hash("hello", 13))
+
+class HashTable{
+    constructor(){
+        this.keyMap = new Array(size)
+    }
+
+    _hash(key){
+        let total = 0
+        let WIERD_PRIME = 31
+        for(let i = 0; i < Math.min(key.length, 100); i++){
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WIERD_PRIME + value) % arrayLen
+        }
+        return total
+    }
+
+    set(key,value){
+        let index = this._hash(key)
+        if(!this.keyMap[index]){
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([])
+
+    }
+
+    get(key){
+        let index = this._hash(key)
+
+        if(this.keyMap[index]){
+            for( let i = 0; i < this.keyMap[index].length; i++){
+                if(this.keyMap[index][i][0] == key){
+                    return this.keyMap[index][i][1]
+                }
+            }
+        }
+        return undefined
+    }
+    
+}
