76dd27ff2f3f88564fe62a8162a79e969609557b
Daniel Gorbachev <dg186533@gmail.com>
2025-01-02 18:58:33 -0500

    added extractmax method to the heap class

--- Data Structures/Heaps/heaps.js
+++ Data Structures/Heaps/heaps.js
@@ -24,11 +24,51 @@ class MaxBinaryHeap{
 
         bubbleup()
     }
+
+    extractMax(){
+        const max = this.values[0]
+        const end = this.values.pop()
+        this.values[0] = end
+        this.sinkDown()
+        return max
+    }
+    sinkDown(){
+        let idx = 0
+        const length = this.values.length
+        const element = this.values[0]
+        while(true){
+            let leftChildIdx = 2 * idx + 1
+            let rightChildIdx = 2 * idx + 2
+            let leftChild, rightChild
+            let swap = null
+
+            if(leftChild < length){
+                leftChild = this.values[leftChildIdx]
+                if(leftChild > element){
+                    swap = leftChild
+                }
+            }
+
+            if(rightChild < length){
+                rightChild = this.values[rightChildIdx]
+                if(rightChild > element){
+                    swap = rightChild
+                }
+            }
+            
+            if(swap === null){
+                break
+            }
+        }
+
+    }
 }
 
 let heap = new MaxBinaryHeap()
 
 heap.insert(55)
+
+heap.extractMax()
 
 console.log(heap)
 
