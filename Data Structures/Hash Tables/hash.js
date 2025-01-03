752c052da64ce0b03d564e6d4d275f931a4c8960
Daniel Gorbachev <dg186533@gmail.com>
2025-01-03 16:33:58 -0500

    started working on hash tables and hash functions

---
+++ Data Structures/Hash Tables/hash.js
@@ -0,0 +1,12 @@
+function hash(key, arrayLen){
+    let total = 0
+    let WIERD_PRIME = 31
+    for(let i = 0; i < Math.min(key.length, 100); i++){
+        let char = key[i]
+        let value = char.charCodeAt(0) - 96
+        total = (total * WIERD_PRIME + value) % arrayLen
+    }
+    return total
+}
+
+console.log(hash("hello", 13))
